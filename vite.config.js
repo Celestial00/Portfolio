ba942601e063550b9db918098b85ecce5004c2ca
Celestial00 <sheerazalee223@gmail.com>
2025-06-27 23:17:27 +0500

    upd05

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  server: {
-    host: true,
-    port: 5173,
-  },
-
+ base: '/',
   plugins: [react(), tailwindcss()],
 });
