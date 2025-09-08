4af91be016b7945d18600a8c401501f0bf956f6d
Celestial00 <sheerazalee223@gmail.com>
2025-09-08 13:44:50 +0500

    /0

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -40,7 +40,7 @@ export default function Tools() {
         className="flex flex-wrap justify-center gap-4"
        
       >
-        <div className="grid grid-cols-2  sm:grid-cols-3 md:grid-cols-4 gap-4">
+        <div className="grid grid-cols-3  sm:grid-cols-3 md:grid-cols-4 gap-4">
           {techStack.map((tool, index) => {
             const Icon = tool.icon;
             return (
