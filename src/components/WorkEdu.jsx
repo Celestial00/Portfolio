aa07e2f7698b8e3b6973be21238159d0ac9b0b12
Celestial00 <sheerazalee223@gmail.com>
2025-09-08 13:10:02 +0500

    minor 1qazchng

--- src/components/WorkEdu.jsx
+++ src/components/WorkEdu.jsx
@@ -23,7 +23,7 @@ export default function WorkEdu({ Title, Place, Date, Desc, project }) {
       </div>
       <p className="text-gray-400 text-sm mt-2"> {Desc} </p>
       <div className="">
-        <p className="text-white dark:text-black mt-2">Project</p>
+        <p className="text-white dark:text-black mt-2 mb-2">Project</p>
         {project.map((pro, index) => (
          <div className="">
            <li key={index} className="mt-1 ml-7 text-sm text-gray-400">
